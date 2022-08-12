82dea455d617953a83ad4bf89c283678ed18a5da
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2022-08-12 08:35:23 +0300

    Update task 2.py

--- task 2.py
+++ task 2.py
@@ -1,71 +1,94 @@
-class Student(): # создаем класс  студент
-    def __init__(self, first_name, last_name, group, *mark): # атрибуты first_name, last_name, group и marks.
+
+class Student():
+    """"
+    Этот класс для подсчета степендии студента.
+    Атрибуты : имя,фамилия,группа ,оценка.
+    """
+    def __init__(self, first_name, last_name, group,mark):
         self.first_name = first_name
         self.last_name = last_name
         self.group = group
         self.mark = mark
         self.marks = []
         self.marks.append(mark)
-
+    """"
+    Конструктор для класса Student.
+    Атрибуты : имя,фамилия,группа ,оценка.
+    """
     def __str__(self):
         return' Student(first name=%s, last name=%s,group=%s,marks=%s)' % (self.first_name,self.last_name,self.group,self.mark)
 
-    # Метод класса
-    def add_mark(self, *mark) :# Принимает оценку и добавляет в marks
-        # Если значение меньше 0 или больше 10, то значение не добавляется
-        if not 10 < mark[0] or mark[0] < 0:
-            return self.marks.append(mark)
+
+    def add_mark(self):
+        if not 10 < self.mark or self.mark  < 0:
+            return self.marks.append(self.mark)
         else:
             return None
+        """""
+        Функция добавления оценки в список оценок.
+        Параметры : оценка.
+        Возращает список с добавленной оценкой.
+        """
 
-
-    # Метод класса
-    def get_average_mark(self):# Возвращает средний балл
+    def get_average_mark(self):
         if len(self.marks) == 0:
             return 0
         else:
-            return sum(self.mark) / len(self.marks)
+            return sum(self.marks) / len(self.marks)
+        """""
+        Функция подсчета средней оценки.
+        Параметры : список оценок.
+        Возращает среднее значение оценок.
+        """
 
-    # Метод класса
-    # Возвращает сумму стипендии.
     def get_scholarship(self):
-        # Если средняя оценка студента равна 5, то сумма 500, иначе 150
-        if self.get_average_mark() == 5 :
+        if self.get_average_mark() == 10 :
             return 500
         else:
             return 150
+        """""
+        Функция подсчета степендии.
+        Параметры : средняя оценка.
+        Возращает сумму степендии.
+        """
 
-class Aspirant(Student): # наследование класса студента
+class Aspirant(Student):
+    """"
+    Этот класс для подсчета степендии аспиранта.
+    Атрибуты : имя,фамилия,группа ,оценка.
+    """
 
-    def __init__(self,first_name, last_name, group,scientific_publications,*mark): # + атрибуты scientific_publications
-        super().__init__(first_name, last_name, group,* mark)
+    def __init__(self,first_name, last_name, group,scientific_publications,mark):
+        super().__init__(first_name, last_name, group,mark)
         self.scientific_publications = scientific_publications
-
+    """"
+    Конструктор для класса Aspirant .
+    Атрибуты : имя,фамилия,группа ,оценка ,научная работа.
+    """
     def __str__(self):
-        return ' Aspirant(first name=%s, last name=%s,group=%s,scientific_publications=%s,marks=%s)' % \
+        return ' Aspirant(first name=%s, last name=%s,group=%s,scientific_publications=%s,mark=%s)' % \
                (self.first_name, self.last_name, self.group,self.scientific_publications,self.mark)
 
-    # переопределение метода класса
-    # Возвращает сумму стипендии.
     def get_scholarship(self):
-        # Если средняя оценка аспиранта равна 5, то сумма 700, иначе 250.
-        if self.get_average_mark() == 5:
+        if self.get_average_mark() == 10:
              return 700
         else:
              return 250
+        """""
+        Функция подсчета степендии.
+        Параметры : средняя оценка.
+        Возращает сумму степендии.
+        """
 
-
-student =  Student('Andrey','BOTY','6',7,9)
+student =  Student('Andrey','BOTY','6',10)
 print(student)
-
-aspirant= Aspirant('Aspirante','BOTY','6','work',2,9)
-print(aspirant)
-
-aspirant.add_mark(3,9)
-student.add_mark(4,8)
-
-
+student.add_mark()
 print(student.get_average_mark())
-print(aspirant.get_average_mark())
 print(student.get_scholarship())
+
+
+aspirant= Aspirant('Aspirante','BOTY','6','work',2)
+print(aspirant)
+aspirant.add_mark()
+print(aspirant.get_average_mark())
 print(aspirant.get_scholarship())
