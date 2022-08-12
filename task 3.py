bf7a4932f3aa4b25642c99556794a11c0703616c
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2022-08-12 09:02:08 +0300

    Update task 3.py

--- task 3.py
+++ task 3.py
@@ -1,24 +1,55 @@
 from typing import Any
+
 class MyString():
+    """"
+      Этот класс для сравнения длин значений.
+      Атрибуты : значений.
+    """
     def __init__(self, value:Any):
         self.value = str(value)
+    """
+        Конструктор для класса MyString .
+        Aтрибуты :value (любой тип данных).
+    """
+
 
     def __eq__(self, other):
         return
         len(self.value) == len(other.value)
-        
+    """
+        Функция сравнения == .
+        Параметры : значение.
+        Возращает результат сравнения .
+    """
+
     def __lt__(self, other):
         return len( self.value) < len(other.value)
-
+    """
+        Функция сравнения < .
+        Параметры : значение.
+        Возращает результат сравнения .
+    """
     def __le__(self, other):
         return len(self.value ) <= len(other.value)
-
+    """
+        Функция сравнения <= .
+        Параметры : значение.
+        Возращает результат сравнения .
+    """
     def __gt__(self, other):
         return len(self.value) > len(other.value)
-
+    """
+        Функция сравнения > .
+        Параметры : значение.
+        Возращает результат сравнения .
+    """
     def __ge__(self, other):
         return len(self.value) >= len(other.value)
-
+    """
+        Функция сравнения >= .
+        Параметры : значение.
+        Возращает результат сравнения .
+    """
 value = MyString('hello')
 value2 = MyString('hi')
 
