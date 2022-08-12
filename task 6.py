5c6c0ef700d2aced02b53369c0535273eb116620
alisa kohanchik <65499196+alisa0040@users.noreply.github.com>
2022-08-12 08:49:49 +0300

    Update task 6.py

--- task 6.py
+++ task 6.py
@@ -5,6 +5,10 @@ class Person():
     name:str
     surname:str
     age:int
+    """"
+      Этот класс для сравнения студентов.
+      Атрибуты : имя,фамилия,возвраст.
+    """
 
 
 liza = Person(name='liza', surname='kohanchik', age=20)
